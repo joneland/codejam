ac3b49980c0a721fcf1c36d343c5e617f857681f
Jon Eland <jonathan.eland@gmail.com>
2014-04-10 22:27:15 +0100

    Template script now reads file in first argument and writes output to file in second argument

--- runner.clj
+++ runner.clj
@@ -1,6 +1,14 @@
 #!/usr/bin/lein exec
 
+(use 'clojure.java.io) 
+
+(defn read-in [file]
+  (line-seq (reader file)))
+
+(defn write-to [file output]
+  (with-open [the-writer (writer file :append true)]
+    (.write the-writer output)))
+
 (let [input-file (nth *command-line-args* 1)
       output-file (nth *command-line-args* 2)]
-  (println input-file)
-  (println output-file))
+   (write-to output-file (str (read-in input-file))))
