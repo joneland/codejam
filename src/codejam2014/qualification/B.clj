7e14a1ac2abc40fa333d10bb2bfeda6f4a4e3eb6
Jon Eland <jonathan.eland@gmail.com>
2015-04-07 20:12:29 +0100

    Fixing formatting

--- src/codejam2014/qualification/B.clj
+++ src/codejam2014/qualification/B.clj
@@ -7,33 +7,33 @@
   (with-open [the-writer (io/writer file :append true)]
     (.write the-writer output)))
 
-(defn read-int[]
+(defn read-int []
   (let [line (read-line)]
     (Integer/parseInt line)))
 
-(defn read-doubles[]
+(defn read-doubles []
   (let [line (read-line)]
     (vec (map #(Double/parseDouble %) (string/split line #"\s+")))))
 
-(defn solve[input]
+(defn solve [input]
   (let [[c f x] input
         no-farms (/ x 2)]
-   (loop [current-time no-farms
-          cps 2
-          farm-penalties 0]
+    (loop [current-time no-farms
+           cps 2
+           farm-penalties 0]
 
-     (let [next-farm-penalty (/ c cps)
-           time-to-reach-x (/ x (+ cps f))
-           next-time (+ farm-penalties next-farm-penalty time-to-reach-x)]
+      (let [next-farm-penalty (/ c cps)
+            time-to-reach-x (/ x (+ cps f))
+            next-time (+ farm-penalties next-farm-penalty time-to-reach-x)]
 
         (if (< current-time next-time)
-         (format "%.7f" current-time)
-        (recur next-time
-               (+ cps f)
-               (+ farm-penalties next-farm-penalty)))))))
+          (format "%.7f" current-time)
+          (recur next-time
+                 (+ cps f)
+                 (+ farm-penalties next-farm-penalty)))))))
 
-(defn -main[]
-	(println "Paste input")
+(defn -main []
+  (println "Paste input")
   (dotimes [x (read-int)]
     (let [input (read-doubles)]
-    (write-to "B_output.txt" (str "Case #" (inc x) ": " (solve input) "\n")))))
+      (write-to "B_output.txt" (str "Case #" (inc x) ": " (solve input) "\n")))))
