de3959110e06388ed411eb64f6fc3d9f5870b6ad
Jon Eland <jonathan.eland@gmail.com>
2015-04-07 20:38:16 +0100

    Adding test case for magic trick problem

--- test/codejam2014/qualification/A_test.clj
+++ test/codejam2014/qualification/A_test.clj
@@ -2,4 +2,11 @@
   (:use expectations)
   (:require [codejam2014.qualification.A :as A]))
 
-(expect nil? nil)
+;;Single matching number from row 1 and 2
+(expect 4 (A/solve [1 2 3 4] [4 5 6 7]))
+
+;;Bad magician when more than 1 matching number in row 1 and 2
+(expect "Bad magician!" (A/solve [1 2 3 4] [3 4 5 6]))
+
+;;Volunteer cheated when no matching number in row 1 and 2
+(expect "Volunteer cheated!" (A/solve [1 2 3 4] [5 6 7 8]))
