1d570e84b0f9834c9021329fd6e99f60bfdfc76a
Jon Eland <jonathan.eland@gmail.com>
2015-04-11 13:57:43 +0100

    Adding solution for 2015 qual - problem A

--- src/codejam2015/qualification/A.clj
+++ src/codejam2015/qualification/A.clj
@@ -4,10 +4,37 @@
     [clojure.string :as string]
     [clojure.java.io :as io]))
 
+(defn indexed-crowd [crowd]
+  (->> (string/split crowd #"")
+       (map #(Integer/parseInt %))
+       (vec)
+       (map-indexed vector)))
+
+(defn crowd-total-before-adding-guests [crowd]
+  (->> (string/split crowd #"")
+       (map #(Integer/parseInt %))
+       (vec)
+       (reduce +)))
+
+(defn crowd-total-after-adding-guests [crowd]
+  (if (= 1 (count crowd))
+    (second (first crowd))
+    (reduce
+      (fn [total shyness-level]
+        (if (< total (first shyness-level))
+          (-> (first shyness-level)
+            (+ (second shyness-level)))
+          (-> (second shyness-level)
+            (+ total))))
+      (first (second crowd))
+      (rest crowd))))
+
 (defn solve
-  "Problem A. XXX"
-  [input]
-  input)
+  "Problem A. Standing Ovation"
+  [crowd]
+  (let [target (crowd-total-after-adding-guests (indexed-crowd crowd))
+        current (crowd-total-before-adding-guests crowd)]
+    (- target current)))
 
 ;; --- infrastructure ---
 (defn write-to [file output]
@@ -21,5 +48,6 @@
 (defn -main []
   (println "Paste input")
   (dotimes [x (read-int)]
-    (let [input (read-line)]
-      (write-to "A_output.txt" (str "Case #" (inc x) ": " (solve input) "\n")))))
+    (let [line (read-line)
+          crowd (second (string/split line #"\s+"))]
+      (write-to "A_output.txt" (str "Case #" (inc x) ": " (solve crowd) "\n")))))
--- test/codejam2015/qualification/A_test.clj
+++ test/codejam2015/qualification/A_test.clj
@@ -3,5 +3,56 @@
   (:require [codejam2015.qualification.A :as A]))
 
 (expect
-  "test"
-  (A/solve "test"))
+  [[0 1] [1 1] [2 1] [3 1] [4 1]]
+  (A/indexed-crowd "11111"))
+
+(expect
+  [[0 1]]
+  (A/indexed-crowd "1"))
+
+
+(expect
+  9
+  (A/crowd-total-before-adding-guests "09"))
+
+(expect
+  4
+  (A/crowd-total-before-adding-guests "110011"))
+
+(expect
+  1
+  (A/crowd-total-before-adding-guests "1"))
+
+(expect
+  3
+  (A/crowd-total-after-adding-guests [[0 1] [1 0] [2 1]]))
+
+(expect
+  5
+  (A/crowd-total-after-adding-guests [[0 1] [1 1] [2 1] [3 1] [4 1]]))
+
+(expect
+  10
+  (A/crowd-total-after-adding-guests [[0 0] [1 9]]))
+
+(expect
+  1
+  (A/crowd-total-after-adding-guests [[0 1]]))
+
+;; Based on sample input vs output at
+;; https://code.google.com/codejam/contest/6224486/dashboard
+(expect
+  0
+  (A/solve "11111"))
+
+(expect
+  1
+  (A/solve "09"))
+
+(expect
+  2
+  (A/solve "110011"))
+
+(expect
+  0
+  (A/solve "1"))
