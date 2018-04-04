54aeb782a8adef841ec0c1f69da3ecf55a71899d
joneland <jonathan.eland@gmail.com>
2018-04-04 13:57:31 +0100

    Move template to specific package

--- java/README.md
+++ java/README.md
@@ -2,7 +2,7 @@
 
 1. Update `build.gradle` run task config to point to main class
 
-`main = 'practice.RunnerTemplate'`
+`main = 'template.Application'`
 
 2. Run following command from command line
 
--- java/src/main/java/template/Application.java
+++ java/src/main/java/template/Application.java
@@ -1,21 +1,17 @@
-package practice;
+package template;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class RunnerTemplate {
+public class Application {
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
         int cases = in.nextInt();
 
         for (int i = 1; i <= cases; ++i) {
             int value = in.nextInt();
-            System.out.println("Case #" + i + ": " + solve(value));
+            System.out.println("Case #" + i + ": " + value);
         }
-    }
-
-    private static int solve(int value) {
-        return value;
     }
 }
